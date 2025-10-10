169c185ae72b811e21c34c5817e9555b23072468
Beloin Sena <beloin.rodrigues@gmail.com>
2025-10-09 22:08:52 -0300

    Created example of gRPC call

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	pb "beloin.com/distributed-cache/internal/network/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const addr = "localhost:50051"
+
 func main() {
-	// Hello World
+	*log.Default() = *log.New(os.Stdout, "", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
+
+	// creds, err := credentials.NewClientTLSFromFile("", "localhost")
+	// dialopt := grpc.WithTransportCredentials(creds)
+	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Could not connect %v", err)
+	}
+	defer conn.Close()
+
+	service := pb.NewGreetServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	req := pb.GreetRequest{Name: "Juan"}
+	resp, err :=service.Greet(ctx, &req)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("Response: %s", resp.GetResult())
+
+	log.Println("Ended client")
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -12,25 +12,29 @@ import (
 const addr = "0.0.0.0:50051"
 
 type Server struct {
-	pb.GreetServiceServer
+	pb.UnimplementedGreetServiceServer
 }
 
-
-
 func main() {
-	*log.Default() = *log.New(os.Stdout, "", log.LstdFlags | log.Lmicroseconds | log.Lshortfile)
+	*log.Default() = *log.New(os.Stdout, "", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
+
+	// TODO: See this
+	// tls.Listen(network string, laddr string, config *tls.Config)
 
 	lis, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatalf("failed to listen on %s: %v\n", addr, err)
 	}
+	defer lis.Close()
 
 	log.Printf("Listening on %s\n", addr)
 
-	s:= grpc.NewServer()
+	s := grpc.NewServer()
+	service := Server{}
+	pb.RegisterGreetServiceServer(s, &service)
 
-	if err = s.Serve(lis); err !=nil {
-		log.Fatalf("Failed to serve %v\n", err)
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve %s\n", err.Error())
 	}
 
 	log.Println("Server stopped suscessfully")
